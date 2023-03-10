684f597bfb02962152c10dfe6d2d72d424d60f06
OlliUlku <olli.ulkun@gmail.com>
2023-03-10 11:19:00 +0200

    Added initial files

---
+++ poscaColors.js
@@ -0,0 +1,146 @@
+//posca colors approx from 2022 chart
+
+let Black = "#231f20";
+let White = "#ffffff";
+let Blue = "#005baa";
+let LightBlue = "#00b9f2";
+let Red = "#ee1d25";
+let Yellow = "#fff200";
+let Green = "#00a652";
+let Pink = "#ef5ba1";
+let Violet = "#75489d";
+let Orange = "#f47920";
+let LightGreen = "#8dc76b";
+let Brown = "#7b4e33";
+let BrightYellow = "#f99e1d";
+let Ivory = "#efdfa0";
+let Beige = "#dbc48e";
+let SlateGrey = "#3e5b70";
+let Grey = "#a7a9ac";
+let RedWine = "#870038";
+let LightOrange = "#faa860";
+let SkyBlue = "#739dd2";
+let Lilac = "#9c8dc4";
+let LightPink = "#f8c1d9";
+let AppleGreen = "#78c244";
+let EmeraldGreen = "#3abb9d";
+let StrawYellow = "#ffe181";
+let Turquoise = "#009794";
+
+let GlacierBlue = "#24abdf";
+let CoralPink = "#e18c98";
+let Lavender = "#c183b9";
+let Apricot = "#fcc287";
+let SunshineYellow = "#fff37c";
+let AquaGreen = "#2fbec2";
+
+let DeepGrey = "#6b6a6f";
+let RubyRed = "#ba1e23";
+let EnglishGreen = "#006c49";
+let PrussianBlue = "#344792";
+let Ochre = "#ffc11e";
+let CacaoBrown = "#7e2c25";
+let Rasperry = "#c84e83";
+let KhakiGreen = "#696f3e";
+let Fuchsia = "#ac2567";
+let DarkRed = "#8c172a";
+let NavyBlue = "#193a52";
+let DarkBrown = "#453132";
+
+let posca = [
+  Black,
+  White,
+  Blue,
+  LightBlue,
+  Red,
+  Yellow,
+  Green,
+  Pink,
+  Violet,
+  Orange,
+  LightGreen,
+  Brown,
+  BrightYellow,
+  Ivory,
+  Beige,
+  SlateGrey,
+  Grey,
+  RedWine,
+  LightOrange,
+  SkyBlue,
+  Lilac,
+  LightPink,
+  AppleGreen,
+  EmeraldGreen,
+  StrawYellow,
+  Turquoise,
+
+  GlacierBlue,
+  CoralPink,
+  Lavender,
+  Apricot,
+  SunshineYellow,
+  AquaGreen,
+
+  DeepGrey,
+  RubyRed,
+  EnglishGreen,
+  PrussianBlue,
+  Ochre,
+  CacaoBrown,
+  Rasperry,
+  KhakiGreen,
+  Fuchsia,
+  DarkRed,
+  NavyBlue,
+  DarkBrown,
+];
+
+let poscaName = [
+  "Black",
+  "White",
+  "Blue",
+  "LightBlue",
+  "Red",
+  "Yellow",
+  "Green",
+  "Pink",
+  "Violet",
+  "Orange",
+  "LightGreen",
+  "Brown",
+  "BrightYellow",
+  "Ivory",
+  "Beige",
+  "SlateGrey",
+  "Grey",
+  "RedWine",
+  "LightOrange",
+  "SkyBlue",
+  "Lilac",
+  "LightPink",
+  "AppleGreen",
+  "EmeraldGreen",
+  "StrawYellow",
+  "Turquoise",
+
+  "GlacierBlue",
+  "CoralPink",
+  "Lavender",
+  "Apricot",
+  "SunshineYellow",
+  "AquaGreen",
+
+  "DeepGrey",
+  "RubyRed",
+  "EnglishGreen",
+  "PrussianBlue",
+  "Ochre",
+  "CacaoBrown",
+  "Rasperry",
+  "KhakiGreen",
+  "Fuchsia",
+  "DarkRed",
+  "NavyBlue",
+  "DarkBrown",
+];
---
+++ sketch.js
@@ -0,0 +1,71 @@
+let madot = []
+let matoCount;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(Beige);
+  angleMode(DEGREES);
+
+  shuffle(posca, true);
+  matoCount = 40; //posca.length
+  for (let i = 0; i < matoCount; i++) {
+    madot[i] = new mato(width / 2, height / 2, posca[i], 360 / matoCount * i)
+  }
+}
+
+function draw() {
+  //background(Beige)
+  for (let i = 0; i < madot.length; i++) {
+    madot[i].update()
+  }
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    for (let i = 0; i < matoCount; i++) {
+      madot[i].stop = !madot[i].stop
+    }
+  }
+}
+class mato {
+  constructor(x, y, _color, rot) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, -0.5);
+    this.vel.rotate(rot)
+    this.acc = createVector(0, 0);
+    this.color = _color;
+    this.rotateAMT = 20;
+    this.size = 10;
+    this.stop = false;
+  }
+
+  update() {
+    // add acc?
+    // this.vel.add(this.acc);
+
+    if (keyIsDown(LEFT_ARROW)) {
+      this.vel.rotate(-this.rotateAMT)
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.vel.rotate(this.rotateAMT)
+    } 
+    if (keyIsDown(UP_ARROW)) {
+      this.size++;
+      //this.rotateAMT--;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      this.size--;
+      //this.rotateAMT++;
+    }
+
+    if (!this.stop) {
+      this.pos.add(this.vel);
+    }
+
+    //show
+    strokeWeight(this.size / 10);
+    noFill();
+    stroke(this.color);
+    point(this.pos.x, this.pos.y);
+  }
+}
